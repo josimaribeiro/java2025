4a9cea364cc821f3b0ea63a60ae1f83b52a95d2f
josimaribeiro <ceatseeduc@gmail.com>
2025-09-25 08:18:21 -0300

    first commit

---
+++ up.cmd
@@ -0,0 +1,7 @@
+echo "# java2025" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:josimaribeiro/java2025.git
+git push -u origin main
