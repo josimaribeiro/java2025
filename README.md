6c7c27b7cbd57dfe100b8da7b29a54ecef915b0c
josimaribeiro <ceatseeduc@gmail.com>
2025-09-30 15:45:30 -0300

    first commit

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-"# java2025" 
-"# java2025" 
-"# java2025" 
-"# java2025" 
-"# java2025" 
-"# java2025" 
-"# java2025" 
-"# java2025" 
-"# java2025" 
-"# java2025" 
-"# $(hostname) - $(date)" 
+# Projeto Java 2025 
+ 
+## Informações do sistema 
+- Nome do computador: LAB2PROF 
+- Usuário: ETEP 
+- Data e hora: 30/09/2025 15:45:30,53 
+ 
+## Descrição 
+Este projeto foi iniciado automaticamente com um script em Windows CMD. 
+ 
+## Autor 
+Josimar Ribeiro 
+ 
--- up.cmd
+++ up.cmd
@@ -1,7 +1,35 @@
-echo "# java2025" >> README.md
+@echo off
+:: Cria README.md com título formatado
+echo # Projeto Java 2025 > README.md
+echo. >> README.md
+echo ## Informações do sistema >> README.md
+echo - Nome do computador: %COMPUTERNAME% >> README.md
+echo - Usuário: %USERNAME% >> README.md
+echo - Data e hora: %date% %time% >> README.md
+echo. >> README.md
+echo ## Descrição >> README.md
+echo Este projeto foi iniciado automaticamente com um script em Windows CMD. >> README.md
+echo. >> README.md
+echo ## Autor >> README.md
+echo Josimar Ribeiro >> README.md
+echo. >> README.md
+
+:: Inicializa o repositório Git
 git init
-git add README.md
+
+:: Adiciona os arquivos ao stage
+git add .
+
+:: Faz o primeiro commit
 git commit -m "first commit"
+
+:: Cria a branch main
 git branch -M main
+
+:: Adiciona o repositório remoto
 git remote add origin git@github.com:josimaribeiro/java2025.git
+
+:: Faz push para o repositório remoto
 git push -u origin main
+
+pause
